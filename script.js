8de8c409eaed3b02826d10091c6969acb48e87c4
Gabrielgqa <gabrielgqa@live.com>
2014-12-19 16:06:37 -0300

    Exc4 batalha correção

--- script.js
+++ script.js
@@ -8,44 +8,46 @@ var batalha = function (atk, def) {
 var person1, person2, life1, life2;
 
 var distribuirPontos = function (name){
-   var person = {Name: name, Force: 0, Armor: 0, Resist: 0};
-   
-   limite1 = 10;
-   person.Force = Math.ceil(Math.random() * 5);
-   limite1 -= person.Force;
-   person.Armor = Math.ceil(Math.random() * 5);
-   limite1 -= person.Armor;
-   if(limite1 > 5)
-       person.Resist = Math.ceil(Math.random() * 5);
-   else
-       person.Resist = Math.ceil(Math.random() * limite1);
+    var person = {Name: name, Force: 0, Armor: 0, Resist: 0};
+    
+    limite1 = 10;
+    person.Force = Math.ceil(Math.random() * 5);
+    limite1 -= person.Force;
+    person.Armor = Math.ceil(Math.random() * 5);
+    limite1 -= person.Armor;
+    if(limite1 > 5)
+        person.Resist = Math.ceil(Math.random() * 5);
+    else
+        person.Resist = Math.ceil(Math.random() * limite1);
 
-   return person;
+    return person;
 }
 
 person1 = distribuirPontos('Caio');
 person2 = distribuirPontos('Gabriel');
 
 var luta = function(person1, person2) {
-   life1 = person1.Resist * 5;
-   life2 = person2.Resist * 5;
-   while (life1 > 0 && life2 > 0) {
-       life2 = life2 - (person1.Force * Math.ceil(Math.random() * 6) - person2.Armor * Math.ceil(Math.random() * 6));
-       if(life2 > 0)
-           life1 = life1 - (person2.Force * Math.ceil(Math.random() * 6) - person1.Armor * Math.ceil(Math.random() * 6));
-   }
-   if(life1>0) {
-       console.log(person1.Name+" ganhou!");
-       console.log("Vida: "+life1+", Força: "+person1.Force+", Armadura: "+person1.Armor+", Resistencia: "+person1.Resist+".");
-       console.log(person2.Name+" perdeu!");
-       console.log("Vida: "+life2+", Força: "+person2.Force+", Armadura: "+person2.Armor+", Resistencia: "+person2.Resist+".");
-   }
-   else {
-       console.log(person2.Name+" ganhou!");
-       console.log("Vida: "+life2+", Força: "+person2.Force+", Armadura: "+person2.Armor+", Resistencia: "+person2.Resist+".");
-       console.log(person1.Name+" perdeu!");
-       console.log("Vida: "+life1+", Força: "+person1.Force+", Armadura: "+person1.Armor+", Resistencia: "+person1.Resist+".");
-   }
+    life1 = person1.Resist * 5;
+    life2 = person2.Resist * 5;
+    while (life1 > 0 && life2 > 0) {
+        life2 = life2 - (person1.Force * Math.ceil(Math.random() * 6) - person2.Armor * Math.ceil(Math.random() * 6));
+        if(life2 > 0)
+            life1 = life1 - (person2.Force * Math.ceil(Math.random() * 6) - person1.Armor * Math.ceil(Math.random() * 6));
+    }
+    if(life1>0) {
+        life2 = 0;
+        console.log(person1.Name+" ganhou!");
+        console.log("Vida: "+life1+", Força: "+person1.Force+", Armadura: "+person1.Armor+", Resistencia: "+person1.Resist+".");
+        console.log(person2.Name+" perdeu!");
+        console.log("Vida: "+life2+", Força: "+person2.Force+", Armadura: "+person2.Armor+", Resistencia: "+person2.Resist+".");
+    }
+    else {
+        life1 = 0;
+        console.log(person2.Name+" ganhou!");
+        console.log("Vida: "+life2+", Força: "+person2.Force+", Armadura: "+person2.Armor+", Resistencia: "+person2.Resist+".");
+        console.log(person1.Name+" perdeu!");
+        console.log("Vida: "+life1+", Força: "+person1.Force+", Armadura: "+person1.Armor+", Resistencia: "+person1.Resist+".");
+    }
 }
 
 luta(person1, person2);
